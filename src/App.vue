caafc3a527b41c143f47ae45ab150a326978fbfd
Marina <95050756+Marindala@users.noreply.github.com>
2023-07-22 19:55:44 -0300

    directivas v-if v-else-if v-else

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 const name = 'Marina'
 const styleColor = 'color:pink'
 //const arrayColor =["red","pink","ARG"] ejemplo con array,
-const activo = true;
+const activo = false;
 </script>
 <!-- etiqueta template renderiza todo el html -->
 <template>
@@ -10,9 +10,13 @@ const activo = true;
   <!--   <h2>{{ arrayColor }}</h2> muestra en texto el array -->
   <h2 :style="styleColor">Background en artes visuales</h2>
   <!-- v:bind:suplanta las {{  }} ///:style(abreviado)-->
-  <h2>
+ <!--  <h2>
     {{ activo ? "Estoy activo" : "No estoy activo" }}
-  </h2>
+  </h2> -->
+  <h2 v-if="activo === true">Sigo...</h2> <!-- Directivas v-if ó v-else -->
+  <h2  v-else-if="activo === false">Yo estoy activa igual</h2> <!-- directiva else tiene que estar seguido del if -->
+<!--   <h2  v-if="!activo">Yo estoy activa igual</h2> -->
+<h2  v-else>y si estas pensando yo sigo activa igual...</h2> 
 </template>
 
 <style>
