c26ba43432ffcf0ed3273ff9c8c9f93e8b85cd39
Marina <95050756+Marindala@users.noreply.github.com>
2023-07-21 22:01:18 -0300

    v:bind:suplanta las {{  }}

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 
 const name = "Marina";
-
+const styleColor = "color:pink"
 </script>
 <!-- etiqueta template renderiza todo el html -->
 <template>
   <h1 class="h1">Hola Soy {{name}} Programadora FullStack 🌀  </h1>
+  <h2 v-bind:style="styleColor">Background en artes visuales</h2> <!-- v:bind:suplanta las {{  }} -->
 </template>
 
 <style>
