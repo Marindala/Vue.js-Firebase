13eada8cd6bc2ad2040ad1bf8ce7c6421a6a4c76
Marina <95050756+Marindala@users.noreply.github.com>
2023-07-22 19:31:48 -0300

    create const array and boolean

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <script setup>
-
-const name = "Marina";
-const styleColor = "color:pink"
+const name = 'Marina'
+const styleColor = 'color:pink'
+//const arrayColor =["red","pink","ARG"] ejemplo con array,
+const activo = true;
 </script>
 <!-- etiqueta template renderiza todo el html -->
 <template>
-  <h1 class="h1">Hola Soy {{name}} Programadora FullStack 🌀  </h1>
-  <h2 v-bind:style="styleColor">Background en artes visuales</h2> <!-- v:bind:suplanta las {{  }} -->
+  <h1 class="h1">Hola Soy {{ name.toUpperCase() }} Programadora FullStack 🌀</h1>
+  <!--   <h2>{{ arrayColor }}</h2> muestra en texto el array -->
+  <h2 :style="styleColor">Background en artes visuales</h2>
+  <!-- v:bind:suplanta las {{  }} ///:style(abreviado)-->
+  <h2>
+    {{ activo ? "Estoy activo" : "No estoy activo" }}
+  </h2>
 </template>
 
 <style>
 h1 {
-  color:blueviolet,
-  
+  color: blueviolet;
 }
 </style>
