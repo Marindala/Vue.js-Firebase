033178dbac98fff13ce04631e845b9f2d268087e
Marina <95050756+Marindala@users.noreply.github.com>
2023-07-23 00:44:26 -0300

    ejercicios + in vue

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,50 @@ const styleColor = 'color:pink'
 //const arrayColor =["red","pink","ARG"] ejemplo con array,
 const activo = true
 const arrayFrutas = ['🍎', '🍌', '🍉', '🟠', '🍓']
+const otherArray = [
+  {
+    name: 'Manzana',
+    price: '5000',
+    description: 'Una linda Manzanita'
+  },
+  {
+    name: 'Banana',
+    price: '7000',
+    description: 'Banana come monito'
+  },
+  {
+    name: 'Naranja',
+    price: '9000',
+    description: 'Vitamina C'
+  }
+]
+
+const objectoFruta = {
+  name: 'Banana',
+  price: '7000',
+  description: 'Banana come monito'
+}
+
+const newArray = [ /* usar directiva que traiga stock superior a 0 */
+  {
+    name: 'Manzana',
+    price: '5000',
+    description: 'Una linda Manzanita',
+    stock:0,
+  },
+  {
+    name: 'Banana',
+    price: '7000',
+    description: 'Banana come monito',
+    stock:20,
+  },
+  {
+    name: 'Naranja',
+    price: '9000',
+    description: 'Vitamina C',
+    stock:10,
+  }
+]
 </script>
 <!-- etiqueta template renderiza todo el html -->
 <template>
@@ -27,9 +71,22 @@ const arrayFrutas = ['🍎', '🍌', '🍉', '🟠', '🍓']
   </h2>
 
   <ul>
-    <li v-for="(fruta, index ) in arrayFrutas" :key="index">{{ fruta }}
-      
-    </li>
+    <li v-for="(fruta, index) in arrayFrutas" :key="index">{{ index }} - {{ fruta }}</li>
+    <!-- :key="id" para recorrer el array en vue se puede usar tanto in como of -->
+  </ul>
+  <ul>
+    <li v-for="(string, i) in otherArray" :key="i">{{ string.name }} -{{ string.description }}</li>
+  </ul>
+
+  <ul>
+    <li v-for="(value, propiedad, index) in objectoFruta" :key="value"> {{ index }}-{{ propiedad }} : {{ value }}</li> <!-- en este caso me renderiza todos los value del object -->
+    
+  </ul>
+
+  <ul>
+    <template v-for="item in newArray" :key="item.name"><!-- como utilizar un <> -->
+     <li v-if="item.stock > 0">{{ item.name }}-{{ item.description }}</li>
+    </template>
   </ul>
 </template>
 
